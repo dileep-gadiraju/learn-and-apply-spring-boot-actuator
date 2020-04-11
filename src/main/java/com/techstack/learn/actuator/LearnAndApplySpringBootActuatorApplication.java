0438187fae5a9c3c07e149903c574d16561ae3d3
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-11 12:38:36 +0530

    Initial Commit: Application generated by Spring Boot Starter Parent

---
+++ src/main/java/com/techstack/learn/actuator/LearnAndApplySpringBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.learn.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndApplySpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAndApplySpringBootActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techstack/learn/actuator/LearnAndApplySpringBootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.learn.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAndApplySpringBootActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
