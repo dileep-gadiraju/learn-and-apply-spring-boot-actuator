b11923bcc93b04926afad9013bcf7cd1539aebf0
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-11 17:08:08 +0530

    Create your custom endpoint as an Actuator Endpoint

---
+++ src/main/java/com/techstack/learn/actuator/actuator/MyCustomEndpoint.java
@@ -0,0 +1,21 @@
+package com.techstack.learn.actuator.actuator;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author Karthikeyan Nithiyanandam
+ */
+@Endpoint(id = "mycustom-endpoint", enableByDefault = true)
+@Component
+public class MyCustomEndpoint {
+
+    @ReadOperation
+    public List<String> getNames() {
+        return List.of("Karthikeyan", "Pascal", "Thomas");
+    }
+
+}
