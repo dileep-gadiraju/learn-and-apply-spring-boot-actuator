76e62c16742b98c865895cf184d001b5d42f7d93
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-11 18:30:36 +0530

    Create a custom Health Indicator and checks it's health.

---
+++ src/main/java/com/techstack/learn/actuator/custom/health/MyCustomHealthIndicator.java
@@ -0,0 +1,24 @@
+package com.techstack.learn.actuator.custom.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+/**
+ * @author Karthikeyan Nithiyanandam
+ */
+@Component
+public class MyCustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        Random random = new Random();
+
+        if(random.nextBoolean()) {
+            return Health.down().withDetail("MSG-001", "Random Failure").build();
+        }
+        return Health.up().build();
+    }
+}
