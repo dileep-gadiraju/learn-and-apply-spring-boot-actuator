bec7bdd2c36b310963ceb7354d34fe3681579919
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-12 21:12:19 +0530

    Implemented custom Gauge metrics for my own logic.

--- src/main/java/com/techstack/learn/actuator/services/ProductServiceImpl.java
+++ src/main/java/com/techstack/learn/actuator/services/ProductServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Karthikeyan Nithiyanandam
@@ -19,26 +20,36 @@ public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
     private JmsTextMessageService jmsTextMessageService;
-    private Counter productCounter;
+    private Counter productCounterForGetProduct;
+    private Counter productCounterForListOfProducts;
+    private AtomicInteger gauge;
 
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository, JmsTextMessageService jmsTextMessageService,
                               MeterRegistry registry) {
         this.productRepository = productRepository;
         this.jmsTextMessageService = jmsTextMessageService;
-        this.productCounter = registry.counter("com.techstack.learn.actuator.services.getproduct");
+        this.productCounterForGetProduct =
+                registry.counter("com.techstack.learn.actuator.services.ProductService.getproduct");
+        this.productCounterForListOfProducts =
+                registry.counter("com.techstack.learn.actuator.services.ProductService.listProducts");
+        this.gauge =
+                registry.gauge("com.techstack.learn.actuator.services.ProductService.pageViewPerMin",
+                        new AtomicInteger(0));
     }
 
     @Override
     public Product getProduct(Integer id) {
         jmsTextMessageService.sendTextMessage("Fetching Product ID: " + id );
-        productCounter.increment();
+        productCounterForGetProduct.increment();
         return productRepository.findById(id).get();
     }
 
     @Override
     public List<Product> listProducts() {
         jmsTextMessageService.sendTextMessage("Listing Products");
+        productCounterForListOfProducts.increment();
+        gauge.set(25); //==> Hardcoded value. Actual logic will be depends on business use case.
         return IteratorUtils.toList(productRepository.findAll().iterator());
     }
 
